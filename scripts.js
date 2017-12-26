fa104322d929dbd6786ffcf8bd8faed6b3ae2e87
AbdallAhmed <ahmed.abdall9@gmail.com>
2017-12-26 13:40:14 -0500

    changinging to materialize CSS

--- scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $(".container").load("testing.html");
-});
-
-$("ul.navbar-nav li").each(function() {
-    $(this).on("click", function(){
-        $(".container").load(($this).attr("data-page")+".html");
-    });
-});
